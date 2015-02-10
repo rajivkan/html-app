e72b28da3c0d9c43c4d3f9984d32e94a16f8cc26
Rajiv <rajiv.ranjan@above-inc.com>
2015-02-10 08:03:22 +0530

    service

--- js/services.js
+++ js/services.js
@@ -17,6 +17,18 @@ appService.service('service', ['$http', '$q', '$filter', function($http, $q, $fi
             error(function(error) {
                 d.reject(error);
             });
+
+            //var urlData = {url: 'http://localhost/test/test.php?name='+$("#txtNm").val()+"&email="+$("#txtPs").val(), method: "GET", crossDomain : true};
+            // var urlData = {url: 'http://localhost/test/test.php', method: "POST", crossDomain : true, data: {name: "test 1", email: "email 1"}};
+            // $.ajax(urlData).
+            // done(function(result){
+            //     //alert(result.toString());
+            //     d.resolve(result);
+            // }).
+            // error(function(error){
+            //     d.reject(error);
+            // });
+
 	        return d.promise;
     	}
 	}]);
